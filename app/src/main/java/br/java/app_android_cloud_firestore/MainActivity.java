71e6beff4bae05d33f2c52b69b8666f560474f79
Diego <diegoteixeirapereira@hotmail.com>
2021-10-08 10:34:28 -0300

    criação do app Cloud Firestore Android.

---
+++ app/src/main/java/br/java/app_android_cloud_firestore/MainActivity.java
@@ -0,0 +1,14 @@
+package br.java.app_android_cloud_firestore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
